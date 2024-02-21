47fa4c9ba73659fe9ae603f3cff15ad4262431ea
Michael de Hoog <michael.dehoog@coinbase.com>
2024-02-20 15:57:20 -1000

    Fix VerifyExclusion / VerifyInclusion inclusion boolean

--- circuits/imt/verifier.go
+++ circuits/imt/verifier.go
@@ -5,25 +5,25 @@ import (
 	"github.com/mdehoog/gnark-circom-smt/circuits/poseidon"
 )
 
+func VerifyExclusion(api frontend.API, enabled, root, key, lowKey, lowValue, lowNextKey, index frontend.Variable, siblings []frontend.Variable) {
+	Verify(api, enabled, root, key, lowKey, lowValue, lowNextKey, index, 0, siblings)
+}
+
 func VerifyInclusion(api frontend.API, enabled, root, key, value, nextKey, index frontend.Variable, siblings []frontend.Variable) {
-	Verify(api, enabled, root, key, key, value, nextKey, index, 0, siblings)
+	Verify(api, enabled, root, key, key, value, nextKey, index, 1, siblings)
 }
 
-func VerifyExclusion(api frontend.API, enabled, root, key, prevKey, value, nextKey, index frontend.Variable, siblings []frontend.Variable) {
-	Verify(api, enabled, root, key, prevKey, value, nextKey, index, 1, siblings)
-}
-
-func Verify(api frontend.API, enabled, root, key, prevKey, value, nextKey, index, inclusion frontend.Variable, siblings []frontend.Variable) {
-	prevKeyEqualsKey := api.IsZero(api.Sub(prevKey, key))
-	AssertEqualIfEnabled(api, prevKeyEqualsKey, inclusion, enabled) // inclusion ? key == prevKey : key != prevKey
+func Verify(api frontend.API, enabled, root, key, lowKey, value, nextKey, index, inclusion frontend.Variable, siblings []frontend.Variable) {
+	prevKeyEqualsKey := api.IsZero(api.Sub(lowKey, key))
+	AssertEqualIfEnabled(api, prevKeyEqualsKey, inclusion, enabled) // inclusion ? key == lowKey : key != lowKey
 	AssertDifferentIfEnabled(api, key, nextKey, enabled)            // key != nextKey
 
-	api.AssertIsLessOrEqual(api.Mul(enabled, prevKey), key)         // prevKey <= key
+	api.AssertIsLessOrEqual(api.Mul(enabled, lowKey), key)          // lowKey <= key
 	nextKeyOverflow := api.Sub(nextKey, api.IsZero(nextKey))        // nextKey == 0 ? nextKey - 1 : nextKey
 	api.AssertIsLessOrEqual(api.Mul(enabled, key), nextKeyOverflow) // key <= nextKey
 
 	indexBin := api.ToBinary(index, len(siblings))
-	h := poseidon.Hash(api, []frontend.Variable{prevKey, value, nextKey})
+	h := poseidon.Hash(api, []frontend.Variable{lowKey, value, nextKey})
 	for level := len(siblings) - 1; level >= 0; level-- {
 		i := indexBin[len(siblings)-1-level]
 		l := api.Select(i, h, siblings[level])
